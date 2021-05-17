81e584a2a837ae5a480eb2dbb55ed26ed813e74e
Nguyen Trung Kien <anhco14@gmail.com>
2021-05-17 15:33:22 +0700

    tran tien

---
+++ src/MyRenderer.js
@@ -0,0 +1,25 @@
+
+
+import { Component } from 'react';
+import { DateTime } from 'react-datetime-bootstrap';
+
+export class MyRenderer extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Minimum Usage to pick today date</h4>
+                <DateTime />
+                <h4>Provide a value</h4>
+                <DateTime value="2017-04-20" />
+                <h4>Format (See momentjs for available formats)</h4>
+                <DateTime pickerOptions={{ format: "LL" }} value="2017-04-20" />
+                <h4>Time Only</h4>
+                <DateTime pickerOptions={{ format: "LTS" }} />
+            </div>
+        )
+    }
+
+}
+
+export default MyRenderer
+
