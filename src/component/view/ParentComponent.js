4c4ca3b389725a53ccb63e5032622cbc6261dcc9
Nguyen Trung Kien <anhco14@gmail.com>
2021-05-17 14:17:50 +0700

    first commit

---
+++ src/component/view/ParentComponent.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import DateTimeField from "react-bootstrap-datetimepicker";
+
+export default class ParentComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: "1990-06-05",
+      format: "YYYY-MM-DD",
+      inputFormat: "DD/MM/YYYY",
+      mode: "date"
+    };
+  }
+
+  handleChange = (newDate) => {
+    console.log("newDate", newDate);
+    return this.setState({date: newDate});
+  }
+
+  render() {
+    const {date, format, mode, inputFormat} = this.state;
+    return (<DateTimeField
+      dateTime={date}
+      format={format}
+      inputFormat={inputFormat}
+      onChange={this.handleChange}
+      viewMode={mode}
+    />);
+  }
+}
