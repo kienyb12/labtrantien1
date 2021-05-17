3f976c1235993565d38cb246af627a3878cda8aa
Nguyen Trung Kien <anhco14@gmail.com>
2021-05-17 15:28:03 +0700

    datetimepicker

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import MyRenderer from './MyRenderer';
 // import ParentComponent from './component/view/ParentComponent';
 
+
 function App() {
-  var DateTimeField = require('react-bootstrap-datetimepicker');
+  // var DateTimeField = require('react-bootstrap-datetimepicker');
 
   return (
     <div className="App">
-      <DateTimeField />
+      {/* <DateTimeField /> */}
+      <MyRenderer />
     </div>
   );
 }
